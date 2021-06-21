3d52e6a43048fe1b242de11641118ce1ce7313b7
junzzan9 <ojs2681@gmail.com>
2021-06-21 09:21:34 +0900

    0621

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -18,24 +18,19 @@ public class GoodsApp {
 				if (key.equals("q")) {
 					for (int i = 0; i < gList.size(); i++) {
 						gList.get(i).showInfo();
+						System.out.println("[입력완료]");
+						System.out.println("======================");
 					}
 					System.out.println("모든상품의 갯수는" + sum + "개 입니다.");
 					break;
 				}
-				int prc;
-				int cnt;
 
-				String[] gInfo = new String[3];
-				Goods g = new Goods();
-				gInfo = key.split(",");
-				prc = Integer.parseInt(gInfo[1]);
-				cnt = Integer.parseInt(gInfo[2]);
-				g.setName(gInfo[0]);
-				g.setPrice(prc);
-				g.setCount(cnt);
+				String[] gInfo =key.split(",");
+				int prc = Integer.parseInt(gInfo[1]);
+				int cnt = Integer.parseInt(gInfo[2]);
+				Goods g = new Goods(gInfo[0],prc,cnt);
 				gList.add(g);
 				sum = sum + cnt;
-				System.out.println("[입력완료]");
 			}
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("입력 오류");
